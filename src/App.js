8f8d2436f149c89192f317ea84ac31aefb00127f
Renan Oliveira <renan.sergipe68@gmail.com>
2021-08-01 12:45:04 -0300

    Trabalhando com useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import Name from "./components/Name";
+import {useState} from "react";
 
 function App(){
+  const [aluno,setAluno,] = useState("Renan")
+
+  function handleChangeName(name){
+   setAluno(name)
+  }
  return(
    <div>
      <h1>teste </h1>
+     <h2>Olá: {aluno}</h2>
+
+      {/*Função anonima para funçoes que recebem parametros */}
+     <button onClick={ ()=> handleChangeName("Param") }> 
+       Mudar nome
+     </button>
+
      <Name 
      name = "renan"
      idade = {30}      
