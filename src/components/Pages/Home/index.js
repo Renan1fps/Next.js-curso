d5fd16d42838b8998d19b2795d0d6cb227176905
Renan Oliveira <renan.sergipe68@gmail.com>
2021-08-18 21:14:01 -0300

    Update index.js

--- src/components/Pages/Home/index.js
+++ src/components/Pages/Home/index.js
@@ -3,6 +3,7 @@ export default function Home(){
     return (
         <div>
             <h1>Home da page</h1>
+            <h2> Linkando paginas </h2>
             <Link to="/sobre"> Sobre</Link> <br/>
             <Link to="/contato"> Contato</Link>     
         </div>
